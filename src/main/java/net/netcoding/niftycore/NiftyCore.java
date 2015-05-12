b24345f442edf7d9a488872fb4573e5ea2ee8c00
Brian Graham <support@netcoding.net>
2015-05-12 17:54:14 -0400

    Now properly loads and caches either NiftyBungee or NiftyBukkit for the MinecraftScheduler

--- src/main/java/net/netcoding/niftycore/NiftyCore.java
+++ src/main/java/net/netcoding/niftycore/NiftyCore.java
@@ -3,31 +3,50 @@ package net.netcoding.niftycore;
 import java.util.logging.Logger;
 
 import net.netcoding.niftycore.reflection.Reflection;
-import net.netcoding.niftycore.util.StringUtil;
 
 public class NiftyCore {
 
 	private static final Logger LOGGER;
 	private static final boolean IS_BUNGEE;
+	private static final Object PLUGIN;
 
 	static {
 		boolean isBungee = false;
+		Object pluginObj = null;
 		Logger logger = Logger.getGlobal();
 
 		try {
-			Class.forName("net.md_5.bungee.api.ProxyServer");
+			Reflection proxy = new Reflection("ProxyServer", "net.md_5.bungee.api");
+			Reflection manager = new Reflection("PluginManager", "net.md_5.bungee.api.plugin");
+			Reflection plugin = new Reflection("Plugin", "net.md_5.bungee.api.plugin");
+			proxy.getClazz();
 			isBungee = true;
-		} catch (Exception ex) { }
+			Object proxyObj = proxy.invokeMethod("getInstance", null);
+			Object managerObj = proxy.invokeMethod("getPluginManager", proxyObj);
+			pluginObj = manager.invokeMethod("getPlugin", managerObj, "NiftyBungee");
+			logger = (Logger)plugin.invokeMethod("getLogger", pluginObj);
+		} catch (Exception ex) {
+			try {
+				Reflection bukkit = new Reflection("Bukkit", "org.bukkit");
+				Reflection manager = new Reflection("PluginManager", "org.bukkit.plugin");
+				Reflection plugin = new Reflection("Plugin", "org.bukkit.plugin");
+				Object managerObj = bukkit.invokeMethod("getPluginManager", null);
+				pluginObj = manager.invokeMethod("getPlugin", managerObj, "NiftyBukkit");
+				logger = (Logger)plugin.invokeMethod("getLogger", pluginObj);
+			} catch (Exception ex2) { }
+		}
 
-		try {
-			String name = StringUtil.format("Nifty{0}", (isBungee() ? "Bungee" : "Bukkit"));
-			Reflection main = new Reflection(name, StringUtil.format("net.netcoding.{0}", name.toLowerCase()));
-			Object mainObj = main.invokeMethod("getPlugin", null);
-			logger = (Logger)main.invokeMethod("getLogger", mainObj);
-		} catch (Exception ex) { }
-
+		PLUGIN = pluginObj;
 		IS_BUNGEE = isBungee;
 		LOGGER = logger;
+	}
+
+	public static final Logger getLogger() {
+		return LOGGER;
+	}
+
+	public static final Object getPlugin() {
+		return PLUGIN;
 	}
 
 	public static final boolean isBungee() {
@@ -36,10 +55,6 @@ public class NiftyCore {
 
 	public static final boolean isBukkit() {
 		return !IS_BUNGEE;
-	}
-
-	public static final Logger getNiftyLogger() {
-		return LOGGER;
 	}
 
 }
