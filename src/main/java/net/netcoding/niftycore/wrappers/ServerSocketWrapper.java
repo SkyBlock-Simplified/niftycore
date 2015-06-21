4222abb64fbd5c3978fa14e7067464134e9a212e
Brian Graham <support@netcoding.net>
2015-06-20 20:42:00 -0400

    Added server socket wrapper so publicly accessible ServerSocket cannot be shut down by outsiders

---
+++ src/main/java/net/netcoding/niftycore/wrappers/ServerSocketWrapper.java
@@ -0,0 +1,63 @@
+package net.netcoding.niftycore.wrappers;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.nio.channels.ServerSocketChannel;
+
+public class ServerSocketWrapper {
+
+	private final ServerSocket serverSocket;
+
+	public ServerSocketWrapper(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+	}
+
+	public ServerSocketChannel getChannel() {
+		return this.serverSocket.getChannel();
+	}
+
+	public InetAddress getInetAddress() {
+		return this.serverSocket.getInetAddress();
+	}
+
+	public int getLocalPort() {
+		return this.serverSocket.getLocalPort();
+	}
+
+	public SocketAddress getLocalSocketAddress() {
+		return this.serverSocket.getLocalSocketAddress();
+	}
+
+	public Socket getSocket() throws IOException {
+		return this.serverSocket.accept();
+	}
+
+	public String getSocketAddress() {
+		return this.serverSocket.getInetAddress().getHostAddress();
+	}
+
+	public int getSoTimeout() throws IOException {
+		return this.serverSocket.getSoTimeout();
+	}
+
+	public boolean isClosed() {
+		return this.serverSocket == null || this.serverSocket.isClosed();
+	}
+
+	public boolean isSocketListening() {
+		return !this.isClosed();
+	}
+
+	public void setReuseAddress(boolean on) throws SocketException {
+		this.serverSocket.setReuseAddress(on);
+	}
+
+	public void setSoTimeout(int timeout) throws SocketException {
+		this.serverSocket.setSoTimeout(timeout);
+	}
+
+}
