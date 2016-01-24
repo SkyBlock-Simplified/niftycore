3251196c04897e5349a11979165d928753696347
Brian Graham <support@netcoding.net>
2016-01-24 03:19:51 -0500

    Added way to ignore characters (because minecraft plays nice with the reset code)

--- src/main/java/net/netcoding/niftycore/util/comparator/LastCharCompare.java
+++ src/main/java/net/netcoding/niftycore/util/comparator/LastCharCompare.java
@@ -1,8 +1,16 @@
 package net.netcoding.niftycore.util.comparator;
 
+import net.netcoding.niftycore.util.concurrent.ConcurrentSet;
+
 import java.util.Comparator;
 
 public class LastCharCompare implements Comparator<String> {
+
+	private final ConcurrentSet<Character> ignoreCharacters = new ConcurrentSet<>();
+
+	public void addIgnoreCharacter(char c) {
+		this.ignoreCharacters.add(c);
+	}
 
 	@Override
 	public int compare(String s1, String s2) {
@@ -10,7 +18,13 @@ public class LastCharCompare implements Comparator<String> {
 		if (s2.length() == 0 && s1.length() > 0) return -1;
 		if (s2.length() == 0 && s1.length() == 0) return 0;
 
-		return s1.charAt(s1.length() - 1) - s2.charAt(s2.length() - 1);
+		char firstChar = s1.charAt(s1.length() - 1);
+		boolean isIgnored = this.ignoreCharacters.contains(firstChar);
+		return firstChar - s2.charAt(s2.length() - 1) * (isIgnored ? -1 : 0);
+	}
+
+	public void removeIgnoredCharacter(char c) {
+		this.ignoreCharacters.remove(c);
 	}
 
 }
