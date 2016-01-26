3ecb07e2195c9d0a3bd0547c48712cd8db8ae689
Brian Graham <support@netcoding.net>
2016-01-26 09:30:29 -0500

    Added color messages

--- src/main/java/net/netcoding/niftycore/mojang/exceptions/ProfileNotFoundException.java
+++ src/main/java/net/netcoding/niftycore/mojang/exceptions/ProfileNotFoundException.java
@@ -1,5 +1,6 @@
 package net.netcoding.niftycore.mojang.exceptions;
 
+import net.netcoding.niftycore.minecraft.ChatColor;
 import net.netcoding.niftycore.mojang.MojangProfile;
 import net.netcoding.niftycore.util.StringUtil;
 
@@ -86,20 +87,20 @@ public class ProfileNotFoundException extends RuntimeException {
 			MojangProfile[] profiles = (MojangProfile[])obj;
 
 			for (MojangProfile profile : profiles)
-				players += StringUtil.format("'{'{0},{1}'}'", profile.getUniqueId(), profile.getName());
+				players += StringUtil.format("'{'{{0}},{{1}}'}'", profile.getUniqueId(), profile.getName());
 
 			return StringUtil.format("The profile data for offline players '{'{0}'}' could not be found!", players);
 		case OFFLINE_PLAYER:
 			MojangProfile profile = (MojangProfile)obj;
-			return StringUtil.format("The profile data for offline player '{'{0},{1}'}' could not be found!", profile.getUniqueId(), profile.getName());
+			return StringUtil.format("The profile data for offline player '{'{{0}},{{1}}'}' could not be found!", profile.getUniqueId(), profile.getName());
 		case UNIQUE_ID:
-			return StringUtil.format("The profile data for uuid {0} could not be found!", obj);
+			return StringUtil.format("The profile data for uuid {{0}} could not be found!", obj);
 		case USERNAMES:
-			return StringUtil.format("The profile data for users '{'{0}'}' could not be found!", StringUtil.implode(", ", (String[])obj));
+			return StringUtil.format("The profile data for users '{'{{0}}'}' could not be found!", StringUtil.implode((ChatColor.GRAY + ", " + ChatColor.RED), (String[])obj));
 		case USERNAME:
-			return StringUtil.format("The profile data for user {0} could not be found!", obj);
+			return StringUtil.format("The profile data for user {{0}} could not be found!", obj);
 		default:
-			return StringUtil.format("The profile data for ''{0}'' could not be found!", obj);
+			return StringUtil.format("The profile data for ''{{0}}'' could not be found!", obj);
 		}
 	}
 
