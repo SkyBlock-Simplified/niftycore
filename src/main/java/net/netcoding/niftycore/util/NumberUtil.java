1bab532eefdc39d8c079d0d22015d9272cdbc1a7
Brian Graham <support@netcoding.net>
2016-01-30 01:35:37 -0500

    Added round methods

--- src/main/java/net/netcoding/niftycore/util/NumberUtil.java
+++ src/main/java/net/netcoding/niftycore/util/NumberUtil.java
@@ -49,4 +49,37 @@ public class NumberUtil {
 		return RANDOM.nextInt((maximum - minimum) + 1) + minimum;
 	}
 
+	/**
+	 * Rounds number to nearest multipleOf value
+	 *
+	 * @param number the number to round
+	 * @param multipleOf multiple to round to
+	 * @return rounded version of number
+	 */
+	public static int round(double number, int multipleOf) {
+		return (int)(Math.round(number / multipleOf) * multipleOf);
+	}
+
+	/**
+	 * Rounds number up to nearest multipleOf value
+	 *
+	 * @param number the number to round up
+	 * @param multipleOf multiple to round to
+	 * @return rounded up version of number
+	 */
+	public static int roundUp(double number, int multipleOf) {
+		return (int)(Math.ceil(number / multipleOf) * multipleOf);
+	}
+
+	/**
+	 * Rounds number down to nearest multipleOf value
+	 *
+	 * @param number the number to round down
+	 * @param multipleOf multiple to round to
+	 * @return rounded down version of number
+	 */
+	public static int roundDown(double number, int multipleOf) {
+		return (int)(Math.floor(number / multipleOf) * multipleOf);
+	}
+
 }
