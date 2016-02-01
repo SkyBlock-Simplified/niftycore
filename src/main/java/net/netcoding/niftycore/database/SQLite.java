f333694bc0b75f431d3fa5970e56991bf506e50a
Brian Graham <support@netcoding.net>
2016-02-01 05:34:09 -0500

    Fixed up sqlite wrapper

--- src/main/java/net/netcoding/niftycore/database/SQLite.java
+++ src/main/java/net/netcoding/niftycore/database/SQLite.java
@@ -1,15 +1,15 @@
 package net.netcoding.niftycore.database;
 
 import net.netcoding.niftycore.database.factory.SQLWrapper;
-import net.netcoding.niftycore.util.StringUtil;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class SQLite extends SQLWrapper {
 
 	public SQLite(File folder, String schema) throws SQLException {
-		super("org.sqlite.JDBC", StringUtil.format("jdbc:sqlite://{0}/{1}.db", (folder.isDirectory() ? folder.getAbsolutePath() : folder.getParent()), schema), "", "");
+		super("org.sqlite.JDBC", "jdbc:sqlite:{0}/{1}", folder, schema, new Properties());
 	}
 
 	/**
@@ -18,7 +18,7 @@ public class SQLite extends SQLWrapper {
 	 * @return True if available, otherwise false.
 	 */
 	@Override
-	public boolean isDriverAvailable() {
+	public final boolean isDriverAvailable() {
 		return super.isDriverAvailable();
 	}
 
