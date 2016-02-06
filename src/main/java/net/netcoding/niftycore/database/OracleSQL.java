45de9048f0a6d4775ae2452f36f6112397869541
Brian Graham <support@netcoding.net>
2016-02-05 20:45:16 -0500

    Renamed to OracleSQL

--- src/main/java/net/netcoding/niftycore/database/OracleSQL.java
+++ src/main/java/net/netcoding/niftycore/database/OracleSQL.java
@@ -5,15 +5,15 @@ import net.netcoding.niftycore.util.StringUtil;
 
 import java.sql.SQLException;
 
-public class Oracle extends SQLWrapper {
+public class OracleSQL extends SQLWrapper {
 
 	public static final int DEFAULT_PORT = 1571;
 
-	public Oracle(String host, String user, String pass, String schema) throws SQLException {
+	public OracleSQL(String host, String user, String pass, String schema) throws SQLException {
 		this(host, DEFAULT_PORT, user, pass, schema);
 	}
 
-	public Oracle(String host, int port, String user, String pass, String schema) throws SQLException {
+	public OracleSQL(String host, int port, String user, String pass, String schema) throws SQLException {
 		super("oracle.jdbc.driver.OracleDriver", StringUtil.format("jdbc:oracle:thin:@{0}:{1,number,#}:{2}", host, port, schema), user, pass);
 	}
 
