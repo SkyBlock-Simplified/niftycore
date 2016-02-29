6db23da006b678153f72dab9fa88a7237395048a
Brian Graham <support@netcoding.net>
2016-02-28 19:14:34 -0500

    Now contains a body if not a system or java error

--- src/main/java/net/netcoding/niftycore/http/exceptions/HttpConnectionException.java
+++ src/main/java/net/netcoding/niftycore/http/exceptions/HttpConnectionException.java
@@ -1,15 +1,16 @@
 package net.netcoding.niftycore.http.exceptions;
 
-import net.netcoding.niftycore.util.StringUtil;
+import net.netcoding.niftycore.http.HttpBody;
 import net.netcoding.niftycore.http.HttpStatus;
+import net.netcoding.niftycore.util.StringUtil;
 
 public class HttpConnectionException extends Exception {
 
+	private final HttpBody body;
 	private final HttpStatus status;
 
-	public HttpConnectionException(HttpStatus status) {
-		super(status.getMessage());
-		this.status = status;
+	public HttpConnectionException(HttpStatus status, HttpBody body) {
+		this(status, body, status.getMessage());
 	}
 
 	public HttpConnectionException(Throwable throwable) {
@@ -18,7 +19,18 @@ public class HttpConnectionException extends Exception {
 
 	public HttpConnectionException(HttpStatus status, Throwable throwable) {
 		super(StringUtil.format("{0}: {1}: {2}", status.getMessage(), throwable.getClass().getName(), throwable.getMessage()), throwable);
+		this.body = HttpBody.EMPTY;
 		this.status = status;
+	}
+
+	private HttpConnectionException(HttpStatus status, HttpBody body, String message) {
+		super(message);
+		this.body = body;
+		this.status = status;
+	}
+
+	public HttpBody getBody() {
+		return this.body;
 	}
 
 	public HttpStatus getStatus() {
