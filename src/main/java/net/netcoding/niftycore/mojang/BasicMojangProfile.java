7aa05a209caf96899a339e25df00a90c887ef3e8
Brian Graham <support@netcoding.net>
2016-04-17 23:10:49 -0400

    Fixed

--- src/main/java/net/netcoding/niftycore/mojang/BasicMojangProfile.java
+++ src/main/java/net/netcoding/niftycore/mojang/BasicMojangProfile.java
@@ -1,6 +1,7 @@
 package net.netcoding.niftycore.mojang;
 
 import net.netcoding.niftycore.minecraft.MinecraftServer;
+import net.netcoding.niftycore.util.json.JsonMessage;
 
 public class BasicMojangProfile extends MojangProfile {
 
@@ -17,6 +18,16 @@ public class BasicMojangProfile extends MojangProfile {
 	@Override
 	public boolean isOnlineAnywhere() {
 		return false;
+	}
+
+	@Override
+	public void sendMessage(JsonMessage message) throws Exception {
+		throw new UnsupportedOperationException("Improperly defined repository resulted in basic profiles!");
+	}
+
+	@Override
+	public void sendMessage(String message) {
+		throw new UnsupportedOperationException("Improperly defined repository resulted in basic profiles!");
 	}
 
 }
