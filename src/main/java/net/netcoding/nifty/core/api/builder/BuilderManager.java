8e7cb9457cb48dc91678d0a66c5ca41720f0ac68
Brian Graham <support@netcoding.net>
2016-07-13 21:39:30 -0400

    Documented everything

--- src/main/java/net/netcoding/nifty/core/api/builder/BuilderManager.java
+++ src/main/java/net/netcoding/nifty/core/api/builder/BuilderManager.java
@@ -6,13 +6,27 @@ import net.netcoding.nifty.core.api.builder.exceptions.RegisteredBuilderExceptio
 import net.netcoding.nifty.core.api.builder.exceptions.UnknownBuilderException;
 import net.netcoding.nifty.core.api.service.exceptions.UnknownServiceException;
 import net.netcoding.nifty.core.util.StringUtil;
+import net.netcoding.nifty.core.util.concurrent.Concurrent;
 import net.netcoding.nifty.core.util.concurrent.ConcurrentSet;
 
+/**
+ * Manager for builders that assist in constructing classes.
+ * <p>
+ * This should be used in an API, and only once across all projects.
+ *
+ * @param <P> Plugin class type used for identification purposes.
+ */
 @SuppressWarnings("unchecked")
 public class BuilderManager<P> {
 
-	private final transient ConcurrentSet<BuilderProvider> BUILDERS = new ConcurrentSet<>();
+	private final transient ConcurrentSet<BuilderProvider> BUILDERS = Concurrent.newSet();
 
+	/**
+	 * Checks if the given service class has a registered builder.
+	 *
+	 * @param service Class type to check.
+	 * @return True if class has a registered builder.
+	 */
 	public final boolean isRegistered(Class<?> service) {
 		Preconditions.checkArgument(service != null, "Service cannot be NULL!");
 
@@ -24,6 +38,16 @@ public class BuilderManager<P> {
 		return false;
 	}
 
+	/**
+	 * Gets the builder provider for the given service class.
+	 *
+	 * @param service Class type to get.
+	 * @param <T> Type of service.
+	 * @param <B> Type of builder class.
+	 * @return Builder provider for the given class.
+	 * @throws UnknownBuilderException When the given service class does not have a registered builder.
+	 * @see #isRegistered(Class)
+	 */
 	public final <T, B extends BuilderCore<T>> BuilderProvider<P, T, B> getBuilderProvider(Class<T> service) throws UnknownBuilderException {
 		if (this.isRegistered(service)) {
 			for (BuilderProvider provider : BUILDERS) {
@@ -34,7 +58,7 @@ public class BuilderManager<P> {
 
 		for (BuilderProvider provider : BUILDERS) {
 			if (provider.getService().isAssignableFrom(service)) {
-				NiftyCore.getLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getBuilder().getName()));
+				NiftyCore.getNiftyLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getBuilder().getName()));
 				return provider;
 			}
 		}
@@ -42,13 +66,23 @@ public class BuilderManager<P> {
 		throw new UnknownServiceException(service);
 	}
 
+	/**
+	 * Gets a new builder instance for the given service class.
+	 *
+	 * @param service Class type to get.
+	 * @param <T> Type of service.
+	 * @param <B> Type of builder class.
+	 * @return Builder instance for the given class.
+	 * @throws UnknownBuilderException When the given service class does not have a registered builder.
+	 * @see #isRegistered(Class)
+	 */
 	public final <T, B extends BuilderCore<T>> B createBuilder(Class<T> service) throws UnknownBuilderException {
 		if (this.isRegistered(service))
 			return (B)this.getBuilderProvider(service).createBuilder();
 
 		for (BuilderProvider provider : BUILDERS) {
 			if (provider.getService().isAssignableFrom(service)) {
-				NiftyCore.getLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getBuilder().getName()));
+				NiftyCore.getNiftyLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getBuilder().getName()));
 				return (B)provider.createBuilder();
 			}
 		}
@@ -56,6 +90,16 @@ public class BuilderManager<P> {
 		throw new UnknownBuilderException(service);
 	}
 
+	/**
+	 * Registers a builder for the given service class.
+	 *
+	 * @param plugin Plugin class used for identification.
+	 * @param service Service class.
+	 * @param builder Builder class.
+	 * @param <T> Type of service.
+	 * @param <B> Type of builder class.
+	 * @throws RegisteredBuilderException When the given service class already has a registered builder.
+	 */
 	public final <T, B extends BuilderCore<T>> void provide(P plugin, Class<T> service, Class<B> builder) throws RegisteredBuilderException {
 		Preconditions.checkArgument(plugin != null, "Plugin cannot be NULL!");
 
--- src/main/java/net/netcoding/nifty/core/api/service/ServiceManager.java
+++ src/main/java/net/netcoding/nifty/core/api/service/ServiceManager.java
@@ -5,14 +5,27 @@ import net.netcoding.nifty.core.NiftyCore;
 import net.netcoding.nifty.core.api.service.exceptions.RegisteredServiceException;
 import net.netcoding.nifty.core.api.service.exceptions.UnknownServiceException;
 import net.netcoding.nifty.core.util.StringUtil;
+import net.netcoding.nifty.core.util.concurrent.Concurrent;
 import net.netcoding.nifty.core.util.concurrent.ConcurrentSet;
-import sun.reflect.Reflection;
 
+/**
+ * Manager for containing services that assist in instance access.
+ * <p>
+ * This should be used in an API, and only once across all projects.
+ *
+ * @param <P> Plugin class type used for identification purposes.
+ */
 @SuppressWarnings("unchecked")
 public class ServiceManager<P> {
 
-	private final transient ConcurrentSet<ServiceProvider> SERVICES = new ConcurrentSet<>();
+	private final transient ConcurrentSet<ServiceProvider> SERVICES = Concurrent.newSet();
 
+	/**
+	 * Checks if the given service class has a registered instance.
+	 *
+	 * @param service Class type to check.
+	 * @return True if class has a registered instance.
+	 */
 	public final boolean isRegistered(Class<?> service) {
 		Preconditions.checkArgument(service != null, "Service cannot be NULL!");
 
@@ -24,6 +37,15 @@ public class ServiceManager<P> {
 		return false;
 	}
 
+	/**
+	 * Gets the builder provider for the given service class.
+	 *
+	 * @param service Class type to get.
+	 * @param <T> Type of service.
+	 * @return Builder provider for the given class.
+	 * @throws UnknownServiceException When the given service class does not have a registered instance.
+	 * @see #isRegistered(Class)
+	 */
 	public final <T> ServiceProvider<T, P> getServiceProvider(Class<T> service) throws UnknownServiceException {
 		if (this.isRegistered(service)) {
 			for (ServiceProvider provider : SERVICES) {
@@ -34,7 +56,7 @@ public class ServiceManager<P> {
 
 		for (ServiceProvider provider : SERVICES) {
 			if (provider.getService().isAssignableFrom(service)) {
-				NiftyCore.getLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getService().getName()));
+				NiftyCore.getNiftyLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getService().getName()));
 				return provider;
 			}
 		}
@@ -42,13 +64,22 @@ public class ServiceManager<P> {
 		throw new UnknownServiceException(service);
 	}
 
+	/**
+	 * Gets the instance for the given service class.
+	 *
+	 * @param service Class type to get.
+	 * @param <T> Type of service.
+	 * @return Builder instance for the given class.
+	 * @throws UnknownServiceException When the given service class does not have a registered instance.
+	 * @see #isRegistered(Class)
+	 */
 	public final <T> T getProvider(Class<T> service) throws UnknownServiceException {
 		if (this.isRegistered(service))
 			return this.getServiceProvider(service).getProvider();
 
 		for (ServiceProvider provider : SERVICES) {
 			if (provider.getService().isAssignableFrom(service)) {
-				NiftyCore.getLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getService().getName()));
+				NiftyCore.getNiftyLogger().warning(StringUtil.format("Service ''{0}'' is superclass of ''{1}''!", service.getName(), provider.getService().getName()));
 				return service.cast(provider.getProvider());
 			}
 		}
@@ -56,15 +87,21 @@ public class ServiceManager<P> {
 		throw new UnknownServiceException(service);
 	}
 
+	/**
+	 * Registers an instance for the given service class.
+	 *
+	 * @param plugin Plugin class used for identification.
+	 * @param service Service class.
+	 * @param instance Instance of service.
+	 * @param <T> Type of service.
+	 * @throws RegisteredServiceException When the given class already has a registered service.
+	 */
 	public final <T> void provide(P plugin, Class<T> service, T instance) throws RegisteredServiceException {
 		Preconditions.checkArgument(plugin != null, "Plugin cannot be NULL!");
 		Preconditions.checkArgument(instance != null, "Instance cannot be NULL!");
 
 		if (this.isRegistered(service))
 			throw new RegisteredServiceException(service);
-
-		for (int i = 0; i < 10; i++)
-			System.out.println("REF CALL: " + Reflection.getCallerClass(i).getName()); // TODO
 
 		SERVICES.add(new ServiceProvider<>(plugin, service, instance));
 	}
